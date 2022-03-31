e4ac70a65f9b2acfc2ea786eb70bc5c81773ccc0
maexx393 <manx393@gmail.com>
2022-03-31 01:11:29 -0400

    Update whitepapers.js

--- src/datas/resources/whitepapers.js
+++ src/datas/resources/whitepapers.js
@@ -5,16 +5,16 @@ export const whitepapers = [
         topic: 'Cryptography and Security',
         title: 'LazyLedger',
         subtitle: 'A Distributed Data Availability Ledger With Client-Side Smart Contracts',
-        downloadUrl: 'https://arxiv.org/pdf/1905.09274.pdf',
-        url: 'https://arxiv.org/abs/1905.09274'
+        downloadUrl: 'https://arxiv.org/pdf/1809.09044.pdf',
+        url: 'https://arxiv.org/pdf/1809.09044.pdf'
     },{
         id: 2,
         category: 'whitepaper',
         topic: 'Cryptography and Security',
         title: 'Fraud and Data Availability Proofs',
         subtitle: 'Maximising Light Client Security and Scaling Blockchains with Dishonest Majorities',
-        downloadUrl: 'https://arxiv.org/abs/1809.09044.pdf',
-        url: 'https://arxiv.org/abs/1809.09044'
+        downloadUrl: 'https://arxiv.org/pdf/1809.09044.pdf',
+        url: 'https://arxiv.org/pdf/1809.09044.pdf'
     },{
         id: 3,
         category: 'whitepaper',
@@ -22,6 +22,6 @@ export const whitepapers = [
         title: 'Securely Scaling Blockchain Base Layers',
         subtitle: '',
         downloadUrl: 'https://discovery.ucl.ac.uk/id/eprint/10117245/1/thesis.pdf',
-        url: 'https://discovery.ucl.ac.uk/id/eprint/10117245/'
+        url: 'https://discovery.ucl.ac.uk/id/eprint/10117245/1/thesis.pdf'
     },
 ]
