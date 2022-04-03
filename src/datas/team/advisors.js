95a61c9a4d6e815782ef783c43c63309737380cf
maexx393 <manx393@gmail.com>
2022-04-03 18:04:29 +0000

    advisors

--- src/datas/team/advisors.js
+++ src/datas/team/advisors.js
@@ -1,100 +1,45 @@
 export const advisors = [
     {
         id : 1,
-        name : 'Morgan Beller',
-        title: 'General Partner, NFX and Co-creator, Diem≋ (fka Libra)',
+        name : 'Advisor 1',
+        title: 'General Partner,  NFT Platform',
         text: '',
         image: 'team/MorganBeller.png',
         socials: {
-            twitter : 'https://twitter.com/beller',
+            twitter : 'https://twitter.com/',
             discord : '',
             github : '',
             email : '',
-            linkedin : 'https://www.linkedin.com/in/morganbeller/',
+            linkedin : 'https://www.linkedin.com/in/',
         }
     },
     {
         id : 2,
-        name : 'Ethan Buchman',
-        title: 'Co-founder, Tendermint and Co-founder, Cosmos',
+        name : 'Advisor 2',
+        title: 'Co-founder, Blockchain contributor',
         text: '',
         image: 'team/EthanBuchman.png',
         socials: {
-            twitter : 'https://twitter.com/buchmanster',
+            twitter : 'https://twitter.com/',
             discord : '',
-            github : 'https://github.com/ebuchman',
+            github : 'https://github.com/',
             email : '',
-            linkedin : 'https://www.linkedin.com/in/ethan-buchman-10b34944/',
+            linkedin : 'https://www.linkedin.com/in/',
         }
     },
     {
         id : 3,
-        name : 'Zaki Manian',
-        title: 'Co-creator, IBC and early Cosmos contributor',
+        name : 'Advisor 3',
+        title: 'Co-creator, Blockchain contributor',
         text: '',
         image: 'team/ZakiManian.png',
         socials: {
-            twitter : 'https://twitter.com/zmanian',
+            twitter : 'https://twitter.com/',
             discord : '',
-            github : 'https://github.com/zmanian',
+            github : 'https://github.com/',
             email : '',
-            linkedin : 'https://www.linkedin.com/in/zmanian/',
+            linkedin : 'https://www.linkedin.com/in/',
         }
     },
-    {
-        id : 4,
-        name : 'James Prestwich',
-        title: 'Founder, Summa (acquired by Celo)',
-        text: '',
-        image: 'team/JamesPrestwich.png',
-        socials: {
-            twitter : 'https://twitter.com/_prestwich',
-            discord : '',
-            github : 'https://github.com/prestwich',
-            email : '',
-            linkedin : 'https://www.linkedin.com/in/prestwich/',
-        }
-    },
-    {
-        id : 5,
-        name : 'George Danezis',
-        title: 'Professor of Security and Privacy Engineering, University College London',
-        text: '',
-        image: 'team/GeorgeDanezis.png',
-        socials: {
-            twitter : 'https://twitter.com/GDanezis',
-            discord : '',
-            github : 'https://github.com/gdanezis',
-            email : '',
-            linkedin : 'https://www.linkedin.com/in/gdanezis/',
-        }
-    },
-    {
-        id : 6,
-        name : 'Matthew Di Ferrante',
-        title: 'Security expert and auditor, Ethereum contributor since 2014',
-        text: '',
-        image: 'team/MatthewDiFerrante.png',
-        socials: {
-            twitter : 'https://twitter.com/matthewdif',
-            discord : '',
-            github : 'https://github.com/mattdf',
-            email : '',
-            linkedin : 'https://www.linkedin.com/in/matthewdiferrante/',
-        }
-    },
-    {
-        id : 7,
-        name : 'Marko Baricevic',
-        title: 'Software Engineer, Interchain Foundation',
-        text: '',
-        image: 'team/MarkoBaricevic.png',
-        socials: {
-            twitter : 'https://twitter.com/mark0baricevic',
-            discord : '',
-            github : 'https://github.com/marbar3778',
-            email : '',
-            linkedin : 'https://www.linkedin.com/in/marko-baricevic-ab0b49214/',
-        }
-    },
+    
 ]
