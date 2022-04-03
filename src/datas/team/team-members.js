20c28687a03f892e7408e1127a95ef6dfd4f933b
maexx393 <manx393@gmail.com>
2022-04-03 17:58:09 +0000

    teams

--- src/datas/team/team-members.js
+++ src/datas/team/team-members.js
@@ -1,239 +1,30 @@
 export const teamMembers = [
     {
         id : 1,
-        name : 'Mustafa Al-Bassam',
+        name : 'Thabang Mashiloane',
         title: 'CEO, Hash Protocol',
         text: 'PhD in blockchain scaling at UCL, Co-founder of Chainspace (acquired by Facebook)',
         image: 'team/MustafaAlBassam.png',
         socials: {
-            twitter : 'https://twitter.com/musalbas',
+            twitter : 'https://twitter.com/cryptosciencist',
             discord : '',
-            github : 'https://github.com/musalbas',
+            github : 'https://github.com/maexx393',
             email : '',
-            linkedin : 'https://www.linkedin.com/in/musalbas/',
+            linkedin : 'https://www.linkedin.com/in/thabangmashiloane/',
         }
     },
     {
         id : 2,
-        name : 'Ismail Khoffi',
+        name : 'Tim Siwula',
         title: 'CTO, Hash Protocol',
-        text: 'Former senior engineer at Tendermint and Interchain Foundation',
+        text: 'Former senior engineer at Consensys',
         image: 'team/IsmailKhoffi.png',
         socials: {
-            twitter : 'https://twitter.com/KreuzUQuer',
+            twitter : 'https://twitter.com/',
             discord : '',
-            github : 'https://github.com/liamsi',
+            github : 'https://github.com/timxor/timxor.github.io',
             email : '',
-            linkedin : 'https://www.linkedin.com/in/ismailkhoffi/',
-        }
-    },
-    {
-        id : 3,
-        name : 'John Adler',
-        title: 'CRO, Hash Protocol',
-        text: 'Creator of Optimistic Rollups, previously scalability researcher at ConsenSys',
-        image: 'team/JohnAdler.png',
-        socials: {
-            twitter : 'https://twitter.com/jadler0',
-            discord : '',
-            github : 'https://github.com/adlerjohn',
-            email : '',
-            linkedin : 'https://www.linkedin.com/in/john-adler/',
-        }
-    },
-    {
-        id : 4,
-        name : 'Nick White',
-        title: 'COO, Hash Protocol',
-        text: 'Co-founder of Harmony, BS & MS from Stanford',
-        image: 'team/NickWhite.png',
-        socials: {
-            twitter : 'https://twitter.com/nickwh8te',
-            discord : '',
-            github : 'https://github.com/nwhite1',
-            email : '',
-            linkedin : 'https://www.linkedin.com/in/nickwh8te/',
-        }
-    },
-    {
-        id : 5,
-        name : 'Tomasz Zdybał',
-        title: 'Software Engineer',
-        text: 'Former core developer at Ethereum Classic, trust minimization maximalist',
-        image: 'team/TomaszZdybal.png',
-        socials: {
-            twitter : 'https://twitter.com/tzdybal',
-            discord : '',
-            github : 'https://github.com/tzdybal',
-            email : '',
-            linkedin : 'https://www.linkedin.com/in/tzdybal/',
-        }
-    },
-    {
-        id : 6,
-        name : 'Hlib Kanunnikov',
-        title: 'Software Engineer',
-        text: 'Previously lead developer at ProximaX, networking enthusiast',
-        image: 'team/HlibKanunnikov.png',
-        socials: {
-            twitter : '',
-            discord : '',
-            github : 'https://github.com/Wondertan',
-            email : '',
-            linkedin : 'https://www.linkedin.com/in/hlib-kanunnikov-ab492116b/',
-        }
-    },
-    {
-        id : 7,
-        name : 'Evan Forbes',
-        title: 'Software Engineer',
-        text: 'Former data engineer, rollup maximalist',
-        image: 'team/EvanForbes.png',
-        socials: {
-            twitter : 'https://twitter.com/evansforbes',
-            discord : '',
-            github : 'https://github.com/evan-forbes',
-            email : '',
-            linkedin : '',
-        }
-    },
-    {
-        id : 8,
-        name : 'Rene Lubov',
-        title: 'Software Engineer',
-        text: 'Former engineer at Go Ethereum with a background in distributed systems testing',
-        image: 'team/ReneLubov.png',
-        socials: {
-            twitter : 'https://twitter.com/renelubov',
-            discord : '',
-            github : 'https://github.com/renaynay',
-            email : '',
-            linkedin : '',
-        }
-    },
-    {
-        id : 9,
-        name : 'Nhu Viet Nguyen',
-        title: 'Software Engineer',
-        text: 'Former testing engineer at Oracle',
-        image: 'team/NhuVietNguyen.png',
-        socials: {
-            twitter : 'https://twitter.com/braveryandglory',
-            discord : '',
-            github : 'https://github.com/Bidon15',
-            email : '',
-            linkedin : 'https://www.linkedin.com/in/nhu-viet-nguyen-a8111093/',
-        }
-    },
-    {
-        id : 11,
-        name : 'Josh Bowen',
-        title: 'Software Engineer',
-        text: 'Former software engineer at Google',
-        image: 'team/JoshBowen.png',
-        socials: {
-            twitter : 'https://twitter.com/Jskybowen',
-            discord : '',
-            github : 'https://github.com/jbowen93',
-            email : '',
-            linkedin : 'https://www.linkedin.com/in/jbowen93/',
-        }
-    },
-    {
-        id : 12,
-        name : 'Alexandru Popescu',
-        title: 'VP Marketing, Hash Protocol',
-        text: 'Former Digital Marketing Wizard at Mindvalley Labs',
-        image: 'team/AlexandruPopescu.png',
-        socials: {
-            twitter : 'https://twitter.com/AlexPopescu082',
-            discord : '',
-            github : '',
-            email : '',
-            linkedin : '',
-        }
-    },
-    {
-        id : 13,
-        name : 'Rachid Chami',
-        title: 'Software Engineer',
-        text: 'Bitcoin believer, security enthusiast, former blockchain developer at Web3 Labs',
-        image: 'team/RachidChami.png',
-        socials: {
-            twitter : '',
-            discord : '',
-            github : 'https://github.com/SweeXordious',
-            email : '',
-            linkedin : 'https://www.linkedin.com/in/rachid-chami/',
-        }
-    },
-    {
-        id : 14,
-        name : 'Aditi Sriram',
-        title: 'Product Manager',
-        text: 'Former product manager at The Graph and ConsenSys, with a background in developer growth',
-        image: 'team/AditiSriram.png',
-        socials: {
-            twitter : 'https://twitter.com/adeets_22',
-            discord : '',
-            github : '',
-            email : '',
-            linkedin : '',
-        }
-    },
-    {
-        id : 15,
-        name : 'Viacheslav Gonkivskyi',
-        title: 'Software Engineer',
-        text: 'Former software engineer at Luxoft',
-        image: 'team/ViacheslavGonkivskyi.png',
-        socials: {
-            twitter : 'https://twitter.com/VGonkivskyi',
-            discord : '',
-            github : 'https://github.com/vgonkivs',
-            email : '',
-            linkedin : 'https://www.linkedin.com/in/vgonkivskyi/',
-        }
-    },
-    {
-        id : 16,
-        name : 'Manav Aggarwal',
-        title: 'Software Engineer',
-        text: 'Former contributor at Nebula Protocol and Two Sigma Investments, BS & MS from Stanford',
-        image: 'team/ManavAggarwal.png',
-        socials: {
-            twitter : 'https://twitter.com/0xnavage',
-            discord : '',
-            github : 'https://github.com/Manav-Aggarwal',
-            email : '',
-            linkedin : 'https://www.linkedin.com/in/manav/',
-        }
-    },
-    {
-        id : 17,
-        name : 'Yaz Khoury',
-        title: 'Head of Developer Relations',
-        text: 'Former DevRel engineer at Celo Foundation and former Director of DevRel at Ethereum Classic',
-        image: 'team/YazKhoury.png',
-        socials: {
-            twitter : 'https://twitter.com/Yazanator',
-            discord : '',
-            github : 'https://github.com/YazzyYaz',
-            email : '',
-            linkedin : '',
-        }
-    },{
-        id : 18,
-        name : 'Ekram Ahmed',
-        title: 'Head of Marketing and Communications',
-        text: 'Former positioning and strategy consultant for Facebook and Google',
-        image: 'team/EkramAhmed.png',
-        socials: {
-            twitter : 'https://twitter.com/ekrahm',
-            discord : '',
-            github : '',
-            email : '',
-            linkedin : '',
+            linkedin : 'https://www.linkedin.com/in/',
         }
     },
 ]
